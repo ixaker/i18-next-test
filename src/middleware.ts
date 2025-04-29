61a366fe18409db7ce7ddd12089e476554a44c00
PHlushkov <pavelgluskov264@gmail.com>
2025-04-29 12:41:20 +0300

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,40 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const locales = ["en", "uk", "ru"];
+const SUPPORTED_LOCALES = ["en", "ru", "uk"];
+const DEFAULT_LOCALE = "en";
+// Это минимальный middleware, который ничего не делает — просто пропускает запрос
+export function middleware(req: NextRequest) {
+  console.log("Попавли в middleware");
 
-function getLocale(req: NextRequest) {
-  const acceptLang = req.headers.get("accept-language");
-  const browserLang = acceptLang?.split(",")[0].split("-")[0] || "en";
+  // Можно залогировать, чтобы убедиться, что middleware сработал
+  const { pathname } = req.nextUrl;
 
-  return locales.includes(browserLang) ? browserLang : "en";
-}
+  console.log("pathname", pathname, req.nextUrl);
 
-export function middleware(request: NextRequest) {
-  const { pathname, origin } = request.nextUrl;
-  // const baseUrl = process.env.BASE_URL;
+  console.log("pathname", pathname);
 
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  );
+  const pathnameIsMissingLocale = !/^\/(en|ru|uk)(\/|$)/.test(pathname);
 
-  if (pathnameHasLocale) return;
+  if (pathnameIsMissingLocale) {
+    const acceptLang = req.headers.get("accept-language");
+    const browserLang =
+      acceptLang?.split(",")[0].split("-")[0] || DEFAULT_LOCALE;
+    const locale = SUPPORTED_LOCALES.includes(browserLang)
+      ? browserLang
+      : DEFAULT_LOCALE;
 
-  const locale = getLocale(request);
+    const redirectPath = `/${locale}${pathname}`;
 
-  console.log("origin", origin);
+    // 👉 принудительно укажем нужный origin вручную (вручную подставляем домен)
+    const fullUrl = new URL(redirectPath, process.env.BASE_URL);
 
-  const redirectPath = `${origin}/${locale}${pathname}`;
+    console.log("redirect to:", fullUrl.toString());
 
-  const fullUrl = new URL(redirectPath, "https://store.qpart.com.ua");
-
-  console.log("fullUrl", fullUrl.toString());
-
-  return NextResponse.redirect(fullUrl);
+    return NextResponse.redirect(fullUrl);
+  }
+  // console.log('next');
+  return NextResponse.next();
 }
 // export const config = {
 //   matcher: [
