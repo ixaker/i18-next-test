18a6be796ace154e26b7bf191a0322eeeb17693f
PHlushkov <pavelgluskov264@gmail.com>
2025-04-29 11:34:44 +0300

    udate middlewaere

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,33 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 
 const locales = ["en", "uk", "ru"];
 
-// Get the preferred locale, similar to the above or using a library
 function getLocale(req: NextRequest) {
   const acceptLang = req.headers.get("accept-language");
   const browserLang = acceptLang?.split(",")[0].split("-")[0] || "en";
 
-  return browserLang;
+  return locales.includes(browserLang) ? browserLang : "en";
 }
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  console.log("pathname", pathname);
+  const { pathname, origin } = request.nextUrl;
 
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
 
-  console.log("pathnameHasLocale", pathnameHasLocale, pathname);
-
   if (pathnameHasLocale) return;
 
   const locale = getLocale(request);
-  request.nextUrl.pathname = `/${locale}${pathname}`;
 
-  return NextResponse.redirect(request.nextUrl);
+  console.log("origin", origin);
+
+  const newUrl = `${origin}/${locale}${pathname}`;
+
+  return NextResponse.redirect(new URL(newUrl));
 }
-
 // export const config = {
 //   matcher: [
 //     // Skip all internal paths (_next)
